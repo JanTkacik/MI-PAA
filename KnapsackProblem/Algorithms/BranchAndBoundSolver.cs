0254de80768ed37db20952bd379047fe9524d1a7
Jan Tkacik <jan.tkacik@gmail.com>
2014-10-29 21:24:09 +0100

    Branch and bound

---
+++ KnapsackProblem/Algorithms/BranchAndBoundSolver.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using KnapsackProblem.Model;
+
+namespace KnapsackProblem.Algorithms
+{
+    class BranchAndBoundSolver : IKnapsackSolver
+    {
+        public int Solve(KnapsackProblemModel problem)
+        {
+            Bag bag = new Bag(problem.BagCapacity);
+
+            List<Item> items = problem.Items.ToList();
+
+            return RecursiveSolver(bag, items, 0);
+        }
+
+        public int RecursiveSolver(Bag bag, List<Item> items, int bestKnown)
+        {
+            if (items.Count > 0)
+            {
+                int bestItemsCost = bestKnown;
+
+                List<Item> recList = new List<Item>(items);
+                recList.RemoveAt(0);
+
+                int sum = recList.Sum(item => item.Cost) + bag.ItemsCost();
+                if (sum >= bestKnown)
+                {
+                    bestItemsCost = RecursiveSolver(bag, recList, bestKnown);
+                }
+                bag.InsertItem(items[0]);
+                if (bag.AcceptableWeight())
+                {
+                    int itemsCost = bag.ItemsCost();
+                    if (itemsCost > bestItemsCost)
+                    {
+                        bestItemsCost = itemsCost;
+                    }
+                }
+                recList = new List<Item>(items);
+                recList.Remove(items[0]);
+                sum = recList.Sum(item => item.Cost) + bag.ItemsCost();
+                if (sum >= bestItemsCost)
+                {
+                    bestItemsCost = RecursiveSolver(bag, recList, bestItemsCost);
+                }
+                bag.RemoveItem(items[0]);
+                return bestItemsCost;
+            }
+            return bestKnown;
+        }
+    }
+}
--- KnapsackProblem/Tests/SolversTests.cs
+++ KnapsackProblem/Tests/SolversTests.cs
@@ -23,6 +23,20 @@ namespace KnapsackProblem.Tests
         }
 
         [Test]
+        public void SimpleTestBnb()
+        {
+            KnapsackProblemModel problem = new KnapsackProblemModel(9000, 100, new List<Item>
+                {
+                    new Item(18, 114, 0), new Item(42, 136, 1), new Item(88, 192, 2), new Item(3, 223, 3)
+                });
+
+            IKnapsackSolver solver = new BranchAndBoundSolver();
+            int solve = solver.Solve(problem);
+
+            Assert.AreEqual(473, solve);
+        }
+
+        [Test]
         public void SimpleTest2()
         {
             KnapsackProblemModel problem = new KnapsackProblemModel(9000, 100, new List<Item>
